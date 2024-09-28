6d77486187c2f1aaa4e546649dc1e1bc342f564f
jackson-kishan <jackson.kishan.56@gmail.com>
2024-09-28 15:31:43 +0600

    Create Laravel 11 Livewire CRUD Application

--- laravel_11_livewire_crud/app/Livewire/Products.php
+++ laravel_11_livewire_crud/app/Livewire/Products.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 use Livewire\Attributes\Locked;
 use Livewire\Attributes\Validate;
@@ -31,6 +32,46 @@ class Products extends Component
         $this->reset("name", "description");
 
         $this->isEdit = false;
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        Product::updateOrCreate(['id' => $this->product_id], [
+          "name" => $this->name,
+          "description" => $this->description,
+        ]);
+
+        session()->flash('message', $this->product_id ? 'Product is updated.' : "Product is added.");
+        $this->resetFields();
+    }
+
+    public function edit($id)
+    {
+        $this->title = "Edit Product";
+
+        $product = Product::findOrFail($id);
+
+        $this->product_id = $id;
+
+        $this->name = $product->name;
+
+        $this->description = $product->description;
+
+        $this->isEdit = true;
+    }
+
+    public function cancel()
+    {
+        $this->resetFields();
+    }
+
+    public function delete($id)
+    {
+        Product::find($id)->delete();
+
+        session()->flash("message", "Product deleted successfully");
     }
 
     public function render()
--- laravel_11_livewire_crud/resources/views/livewire/products.blade.php
+++ laravel_11_livewire_crud/resources/views/livewire/products.blade.php
@@ -1,3 +1,57 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="row justify-content-center mt-3">
+    <div class="col-md-12">
+
+        @if (session()->has('message'))
+            <div class="alert alert-success" role="alert">
+                {{ session('message') }}
+            </div>
+        @endif
+
+        @include('livewire.updateOrCreate')
+
+        <div class="card">
+            <div class="card-header">Product List</div>
+            <div class="card-body">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                      <tr>
+                        <th scope="col">S#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                            <tr wire:key="{{ $product->id }}">
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td>{{ $product->name }}</td>
+                                <td>{{ $product->description }}</td>
+                                <td>
+                                    <button wire:click="edit({{ $product->id }})" 
+                                        class="btn btn-primary btn-sm">
+                                        <i class="bi bi-pencil-square"></i> Edit
+                                    </button>   
+
+                                    <button wire:click="delete({{ $product->id }})" 
+                                        wire:confirm="Are you sure you want to delete this product?"
+                                        class="btn btn-danger btn-sm">
+                                        <i class="bi bi-trash"></i> Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4">
+                                    <span class="text-danger">
+                                        <strong>No Product Found!</strong>
+                                    </span>
+                                </td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>    
 </div>
