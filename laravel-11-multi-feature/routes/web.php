7329bb95eae5a787c6eb04a33ec9f6400d6dc978
jackson-kishan <jackson.kishan.56@gmail.com>
2025-06-30 19:28:39 +0600

    creating like dislike system from itsolutionstuff - create route for like and dislike

--- laravel-11-multi-feature/routes/web.php
+++ laravel-11-multi-feature/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\LikeDislikeController;
 use App\Http\Controllers\UploadImageController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,9 @@ Route::post('users/create', [FormController::class, 'store'])->name('users.store
 // Image Upload
 Route::get('upload', [UploadImageController::class, 'index']);
 Route::get('upload', [UploadImageController::class, 'store']);
+
+// Like Dislike System
+Route::middleware('auth')->group(function () {
+  Route::get('/posts',[LikeDislikeController::class, 'index'])->name('ld.index');
+  Route::post('/posts/like-dislike',[LikeDislikeController::class, 'likes'])->name('ld.store');
+});
