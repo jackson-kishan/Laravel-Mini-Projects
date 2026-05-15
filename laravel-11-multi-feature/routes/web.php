64da8f96c487c6dcf62bda9ff11710d034a76db6
Kishan Rozario <jackson.kishan.56@gmail.com>
2026-05-15 23:21:13 +0600

    Update: add acd to cart route listing

--- laravel-11-multi-feature/routes/web.php
+++ laravel-11-multi-feature/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\FormController;
 use App\Http\Controllers\LikeDislikeController;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\NotificationPostController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\QrCodeController;
 use App\Http\Controllers\StripePaymentController;
 use App\Http\Controllers\UploadImageController;
@@ -52,6 +53,13 @@ Route::middleware(['auth', 'userActivity'])->group(function () {
   //Send Mail using Queue
   Route::get('/send-mail', [MailController::class, 'index'])->name('send.mail.index');
 });
+
+// Add to Cart
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/cart', [ProductController::class, 'cart'])->name('cart.index');
+Route::post('/add-to-cart/{id}', [ProductController::class, 'addToCart'])->name('add.to.cart');
+Route::patch('/update-cart', [ProductController::class, 'update'])->name('update.cart');
+Route::delete('/remove-from-cart', [ProductController::class, 'remove'])->name('remove.from.cart');
 
 //Stripe Payment
 Route::controller(StripePaymentController::class)->group(function() {
